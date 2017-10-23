1242ce53f7f81c2bda6205c95093602d20392e8a
joel <joelhanson025@gmail.com>
2017-10-23 11:01:37 +0530

    template completed.

--- protwo/apptwo/views.py
+++ protwo/apptwo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
 def index(request):
-	return HttpResponse("<h1>Hi</h1>")
+	my_dict = {'me':"Hello i am binding index",'shit':"hello"}
+	return render(request,'apptwo/index.html',context = my_dict)
