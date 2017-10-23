434df96b038b6ab3fe2b88dd0d931166b623341c
joel <joelhanson025@gmail.com>
2017-10-23 13:45:41 +0530

    static files are being uploaded and rendered

--- protwo/apptwo/views.py
+++ protwo/apptwo/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
+from django.db import models
 def index(request):
-	my_dict = {'me':"Hello i am binding index",'shit':"hello"}
+	my_dict = {'me':"Hello i am binding index",'shit':"Hello"}
 	return render(request,'apptwo/index.html',context = my_dict)
+
+def help(request):
+	my_dict = {'me':"help"}
+	return render(request,'apptwo/help.html',context = my_dict)
