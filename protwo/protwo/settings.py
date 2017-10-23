b994b65ed609f576345c9e5bce0a03bb34aae75e
joel <joelhanson025@gmail.com>
2017-10-23 16:45:32 +0530

    Question and choice added.

--- protwo/protwo/settings.py
+++ protwo/protwo/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'apptwo',
+	# 'apptwo',
+	'apptwo.apps.ApptwoConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'protwo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'joel',
+		'USER': 'admin',
+		'PASSWORD': 'tms@123',
+		'HOST': '127.0.0.1',
+		'PORT': '5432',
     }
 }
 
