efeddc0a4abf0bf069c3f8e36cfad5b92b334578
joel <joelhanson025@gmail.com>
2017-10-25 10:46:56 +0530

    filter done and made filter

--- prothree/appthree/templatestag/extra.py
+++ prothree/appthree/templatestag/extra.py
@@ -1,10 +1,13 @@
 from django import template
 
 register = template.Library()
-
+@register.filter(name='cut')
 def cut(value,arg):
 	"""
 	The cut out all values
 	"""
 	return value.replace(arg,'')
-register.filter('cut',cut)
+# register.filter('cut',cut)
+
+
+# using decorator
