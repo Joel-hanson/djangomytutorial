8664c3c82123c8a2d29dfb3580b632719ade501f
joel <joelhanson025@gmail.com>
2017-10-27 17:10:23 +0530

    done CRUD done for school.

--- advan/advanapp/models.py
+++ advan/advanapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 # Create your models here.
 class School(models.Model):
 	name = models.CharField(max_length=256)
@@ -8,7 +8,8 @@ class School(models.Model):
 
 	def __str__(self):
 		return self.name
-
+	def get_absolute_url(self):
+		return reverse("advanapp:detail",kwargs={'pk':self.pk})
 class Student(models.Model):
 	name = models.CharField(max_length=256)
 	age = models.PositiveIntegerField()
--- advan/advanapp/views.py
+++ advan/advanapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View,TemplateView,ListView,DetailView
+from django.views.generic import View,CreateView,UpdateView,DeleteView,TemplateView,ListView,DetailView
+from django.core.urlresolvers import reverse_lazy
+
 # Create your views here.
 # def index(request):
 	# return render(request,'advanapp/index.html',{"data":request.GET})
@@ -24,6 +26,19 @@ class SchoolListView(ListView):
 
 class SchoolDetailView(DetailView):
 	#school
-	context_object_name = 'school_test'
+	context_object_name = 'school_detail'
 	model = models.School
 	template_name = 'advanapp/school_details.html'
+
+class SchoolCreateView(CreateView):
+	fields = ('name','principle','location')
+	model = models.School
+
+class SchoolUpdateView(UpdateView):
+	fields = ('name','principle')
+	model = models.School
+
+
+class SchoolDeleteView(DeleteView):
+	model = models.School
+	success_url = reverse_lazy("advanapp:list")
